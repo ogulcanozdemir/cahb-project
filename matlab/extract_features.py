a42b5ac252408c538ff44b657745e85f40bd6670
alperkamil <alperkamil.bozkurt@gmail.com>
2016-12-18 09:47:15 +0200

    Add the list of the finished features.

--- matlab/extract_features.py
+++ matlab/extract_features.py
@@ -10,14 +10,31 @@ videoPath = dirPath + os.sep + 'charades-data' + os.sep
 featurePath = dirPath + os.sep + 'charades-features' + os.sep
 executable = dirPath + os.sep + 'DenseTrackStab'
 
-def extract(idx, file_path):
-    print str(idx) + " : " + file_path;
-    file_name = os.path.basename(file_path)
-    sp = subprocess.Popen([executable, file_path], stdout=subprocess.PIPE)
-    dest = featurePath + file_name[:-4] + '.features.gz'
+
+def extract(idx, filePath):
+    print str(idx) + " : " + filePath
+    fileName = os.path.basename(filePath)
+    sp = subprocess.Popen([executable, filePath], stdout=subprocess.PIPE)
+    dest = featurePath + fileName[:-4] + '.features.gz'
     with gzip.open(dest, 'wb') as f:
         f.write(sp.stdout.read())
 
+
+def getFinishedVideoNames():
+    finishedTxt = 'first_charades_features.txt'
+    finishedVideoNames = []
+    for line in open(finishedTxt):
+        finishedVideoNames.append(line[-18:-13])
+    return finishedVideoNames[2:]  # Ignore `.` and `..` case.
+
+
 if __name__ == '__main__':
-    num_cores = multiprocessing.cpu_count()
-    results = Parallel(n_jobs=num_cores)(delayed(extract)(idx, name) for idx, name in enumerate(glob(videoPath + '*.mp4')))
+    finishedVideoNames = getFinishedVideoNames()
+    videoPathList = glob(videoPath + '*.mp4')
+    unfinishedVideoPathList = []
+    for videoPath in videoPathList:
+        if videoPath[-9:-4] not in finishedVideoNames:
+            unfinishedVideoPathList.append(videoPath)
+    numCores = multiprocessing.cpu_count()
+    results = Parallel(n_jobs=numCores)(
+        delayed(extract)(idx, name) for idx, name in enumerate(unfinishedVideoPathList))
